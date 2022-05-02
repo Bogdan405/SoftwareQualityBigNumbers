cdb254e7e0d91aeea02832d704bd42e6f2fdb304
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2022-05-02 13:50:57 +0300

    Finished Big Numbers

--- big_numbers/big_number.py
+++ big_numbers/big_number.py
@@ -1,33 +1,197 @@
+from big_numbers import errors
+
+
 class BigNumber:
 
     def __init__(self, big_number_string: str, max_size: int):
-        self.big_number = big_number_string
+        self.validate_big_number_string(big_number_string, max_size)
+        self.big_number_array = [int(c) for c in reversed(big_number_string)]
+        self.max_size = max_size
+
+    @property
+    def value(self):
+        return "".join([str(x) for x in reversed(self.big_number_array)])
+
+    def __eq__(self, other):
+        if len(self.big_number_array) != len(other.big_number_array):
+            return False
+        for index in range(len(self.big_number_array)):
+            if self.big_number_array[index] != other.big_number_array[index]:
+                return False
+        return True
+
+    def __le__(self, other):
+        if len(self.big_number_array) > len(other.big_number_array):
+            return False
+        if len(self.big_number_array) < len(other.big_number_array):
+            return True
+
+        for index in reversed(range(len(self.big_number_array))):
+            if self.big_number_array[index] > other.big_number_array[index]:
+                return False
+            if self.big_number_array[index] < other.big_number_array[index]:
+                return True
+        return True
+
+    def __lt__(self, other):
+        if len(self.big_number_array) > len(other.big_number_array):
+            return False
+        if len(self.big_number_array) < len(other.big_number_array):
+            return True
+
+        for index in reversed(range(len(self.big_number_array))):
+            if self.big_number_array[index] > other.big_number_array[index]:
+                return False
+            if self.big_number_array[index] < other.big_number_array[index]:
+                return True
+        return False
+
+    def __repr__(self):
+        return self.value
+
+    def __getitem__(self, key):
+        return self.big_number_array[key]
+
+    def __setitem__(self, key, value):
+        self.big_number_array[key] = value
+
+    def __copy__(self):
+        return BigNumber(self.value, self.max_size)
+
+    def __str__(self):
+        return self.value
 
     def __add__(self, other: "BigNumber"):
-        pass
+        if len(self.big_number_array) < len(other.big_number_array):
+            result = other.__copy__()
+            b = self
+        else:
+            result = self.__copy__()
+            b = other
+
+        carry = 0
+
+        for index in range(len(result.big_number_array)):
+            result[index] += carry
+            if index < len(b.big_number_array):
+                result[index] += b[index]
+            carry = result[index] // 10
+            result[index] = result[index] % 10
+
+        if carry:
+            result.big_number_array.append(carry)
+
+        if result.max_size < len(result.big_number_array):
+            raise errors.NumberSizeGreaterThanLimit(result.value, result.max_size)
+
+        return result
 
     def __sub__(self, other: "BigNumber"):
-        pass
+        if len(self.big_number_array) < len(other.big_number_array):
+            raise errors.NegativeResult(self.value, other.value, "-")
+
+        result = self.__copy__()
+        b = other
+
+        carry = 0
+        for index in range(len(result.big_number_array)):
+            result[index] += carry
+            if index < len(b.big_number_array):
+                result[index] -= b[index]
+            carry = result[index] // 10
+            result[index] = result[index] % 10
+
+        if carry:
+            raise errors.NegativeResult(self.value, other.value, "-")
+
+        while not result[-1] and len(result.big_number_array) > 1:
+            result.big_number_array = result[:-1]
+
+        return result
 
     def __mul__(self, other: "BigNumber"):
-        pass
+        one = BigNumber("1", self.max_size)
+        b = other.__copy__()
+        result = BigNumber("0", self.max_size)
+        while b.value != "0":
+            result = result + self
+            b = b - one
+        return result
 
     def __truediv__(self, other: "BigNumber"):
-        pass
+        if len(self.big_number_array) < len(other.big_number_array):
+            raise errors.NegativeResult(self.value, other.value, "-")
+
+        if other.value == "0":
+            raise errors.DivisionByZero(self, other)
+        one = BigNumber("1", self.max_size)
+        a = self.__copy__()
+        result = BigNumber("0", self.max_size)
+        while True:
+            try:
+                a = a - other
+            except errors.NegativeResult:
+                break
+            result = result + one
+
+        return result
 
     def __pow__(self, power: "BigNumber", modulo=None):
-        pass
+        one = BigNumber("1", self.max_size)
+        b = power.__copy__()
+        result = BigNumber("1", self.max_size)
+        while b.value != "0":
+            result = result * self
+            b = b - one
+        return result
 
     def square_root(self):
-        pass
+
+        zero = BigNumber("0", self.max_size)
+        two = BigNumber("2", self.max_size)
+        one = BigNumber("1", self.max_size)
+
+        if self == zero:
+            return zero
+
+        left = one.__copy__()
+        right = self.__copy__()
+        v = one.__copy__()
+        mid = zero.__copy__()
+
+        while left <= right:
+            mid = mid + left
+            mid = mid + right
+
+            mid = mid / two
+            prod = mid * mid
+
+            if prod <= self:
+                v = mid.__copy__()
+                mid = mid + one
+                left = mid
+            else:
+                mid = mid - one
+                right = mid
+
+            mid = zero.__copy__()
+        return v
 
     @staticmethod
-    def is_valid_big_number_string(big_number_string: str):
-        pass
+    def validate_big_number_string(big_number_string: str, max_size: int):
+        if big_number_string[0] == '0' and len(big_number_string) > 1:
+            raise errors.ZeroStartingNumberString()
 
-    class NegativeResult(ValueError):
-        pass
+        valid_characters = "0123456789"
+        for c in big_number_string:
+            if c not in valid_characters:
+                raise errors.InvalidCharInNumberString(c)
 
-    class InvalidCharInNumberString(ValueError):
-        pass
-    
+        if len(big_number_string) > max_size:
+            raise errors.NumberSizeGreaterThanLimit(big_number_string, max_size)
+
+
+if __name__ == '__main__':
+    x = BigNumber("1000000", 30)
+    y = BigNumber("0", 30)
+    print(x.square_root())
---
+++ big_numbers/errors.py
@@ -0,0 +1,26 @@
+class NegativeResult(ValueError):
+    def __init__(self, value1, value2, operator):
+        super(NegativeResult, self).__init__(f"The resulted big number would be negative: {value1} {operator} {value2}")
+
+
+class InvalidCharInNumberString(ValueError):
+    def __init__(self, invalid_character):
+        super(InvalidCharInNumberString, self).__init__(
+            f"The number string contains the invalid character '{invalid_character}'")
+
+
+class ZeroStartingNumberString(ValueError):
+    def __init__(self):
+        super(ZeroStartingNumberString, self).__init__("The number string starts with zero")
+
+
+class NumberSizeGreaterThanLimit(ValueError):
+    def __init__(self, big_number_string, max_size):
+        super(NumberSizeGreaterThanLimit, self).__init__(
+            f"The number string {big_number_string} is bigger than the allowed limit {max_size}")
+
+
+class DivisionByZero(ValueError):
+    def __init__(self, a, b):
+        super(DivisionByZero, self).__init__(
+            f"Cannot divide by 0 in: {a} / {b}")
