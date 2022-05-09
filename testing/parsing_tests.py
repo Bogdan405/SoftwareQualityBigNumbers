61fd0041834eb8200594600ffa381358caf6cfe9
Minut Mihai Dimitrie <61323097+Elaech@users.noreply.github.com>
2022-05-10 00:01:58 +0300

    Created Test Cases for Parsing and UDialog

--- testing/parsing_tests.py
+++ testing/parsing_tests.py
@@ -1,6 +1,8 @@
 import unittest
 
 from parsing.postfix_expression import PostfixExpression
+from parsing import errors as perrors
+from big_numbers import errors as berrors
 
 
 class TestBuildPostFixedExpression(unittest.TestCase):
@@ -8,11 +10,57 @@ class TestBuildPostFixedExpression(unittest.TestCase):
     def setUp(self) -> None:
         self.expression = PostfixExpression("3+3")
 
-    def test_accepts_null(self):
-        self.expression.build_post_fixed_expression("")
+    def test_doesnt_accepts_null(self):
+        self.failUnlessRaises(ValueError, self.expression.build_post_fixed_expression, "")
+
+    def test_doesnt_accept_illegal_symbols(self):
+        try:
+            self.failUnlessRaises(ValueError, self.expression.build_post_fixed_expression("3+$4"))
+        except Exception as e:
+            self.fail(f"Generated error {e.__class__} {e}")
+
+    def test_accepts_incorrect_operations_syntax(self):
+        self.expression.build_post_fixed_expression("3+(4*5)*3-4")
+        # self.failUnlessRaises()
 
 
-TEST_CASES = [TestBuildPostFixedExpression]
+class TestSolveExpression(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+class TestRestorePostfixedToString(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+class TestImportXML(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+class TestExportXML(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+class TestShowSolvingHistory(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+class TestMainExample(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+class TestInitExpression(unittest.TestCase):
+    def setUp(self) -> None:
+        self.expression = PostfixExpression("3+3")
+
+
+TEST_CASES = [TestBuildPostFixedExpression, TestSolveExpression, TestImportXML, TestExportXML,
+              TestShowSolvingHistory, TestMainExample, TestInitExpression, TestRestorePostfixedToString]
 
 
 def main():
--- testing/user_dialog_tests.py
+++ testing/user_dialog_tests.py
@@ -1,13 +1,61 @@
 import unittest
+from user_dialog.user_dialog import UserDialog
+from parsing.postfix_expression import PostfixExpression
+from parsing import errors
+from big_numbers import errors
 
 
 class TestMainHelp(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
 
     def test_prints_something(self):
         pass
 
 
-TEST_CASES = [TestMainHelp]
+class TestShowCurrentExpression(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestAutomaticMenu(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestChangeCurrentExpressionXML(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestExportCurrentExpressionSML(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestInteractiveMenu(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestChangeNumberSize(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestModifyVerbosity(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+class TestSolveCurrentExpression(unittest.TestCase):
+    def setUp(self) -> None:
+        UserDialog.current_expression = PostfixExpression("3+3")
+
+
+TEST_CASES = [TestMainHelp, TestSolveCurrentExpression, TestModifyVerbosity, TestChangeCurrentExpressionXML,
+              TestExportCurrentExpressionSML, TestInteractiveMenu, TestChangeNumberSize, TestShowCurrentExpression,
+              TestAutomaticMenu]
 
 
 def main():
