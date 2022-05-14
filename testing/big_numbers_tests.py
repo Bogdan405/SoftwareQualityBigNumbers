3f33d302dde431f8200098ef42098be76cbaa2aa
Bogdan405 <Bogdan405@outlook.com>
2022-05-14 13:45:10 +0300

    increased test coverage

--- testing/big_numbers_tests.py
+++ testing/big_numbers_tests.py
@@ -67,8 +67,89 @@ class TestBigNumberOperations(unittest.TestCase):
         number1 = BigNumber("9000000000", 10)
         number2 = BigNumber("9000000000", 10)
 
-        with self.assertRaises(errors.NumberSizeGreaterThanLimit):
+        with self.assertRaises(errors.NumberSizeGreaterThanLimit,msg="Add overflow result failed"):
             number1+number2
+
+    def test_sub_simple(self):
+        number1 = BigNumber("9000000300", 10)
+        number2 = BigNumber("3000000100", 10)
+        number3 = BigNumber("6000000200", 10)
+
+        self.assertEqual(number1 - number2, number3, "Number sub simple failed")
+
+    def test_sub_negative(self):
+        number1 = BigNumber("3000000000", 10)
+        number2 = BigNumber("3000000100", 10)
+
+        with self.assertRaises(errors.NegativeResult,msg="Sub negative result failed"):
+            number1 - number2
+
+    def test_mul_simple(self):
+        number1 = BigNumber("1111", 10)
+        number2 = BigNumber("1111", 10)
+        number3 = BigNumber("1234321", 10)
+
+        self.assertEqual(number1 * number2, number3, "Number mul simple failed")
+
+    def test_mul_overflow(self):
+        number1 = BigNumber("3000000000", 10)
+        number2 = BigNumber("3000000100", 10)
+
+        with self.assertRaises(errors.NumberSizeGreaterThanLimit,msg="Mul overflow result failed"):
+            number1 * number2
+
+    def test_div_simple(self):
+        number1 = BigNumber("10000000", 10)
+        number2 = BigNumber("100", 10)
+        number3 = BigNumber("100000", 10)
+
+        self.assertEqual(number1 / number2, number3, "Number div simple failed")
+
+    def test_div_0(self):
+        number1 = BigNumber("30000000", 10)
+        number2 = BigNumber("0", 10)
+
+        with self.assertRaises(errors.DivisionByZero, msg="Div 0 result failed"):
+            number1 / number2
+
+    def test_pow_simple(self):
+        number1 = BigNumber("100", 10)
+        number2 = BigNumber("3", 10)
+        number3 = BigNumber("1000000", 10)
+
+        self.assertEqual(pow(number1,number2), number3, "Number pow simple failed")
+
+    def test_pow_overflow(self):
+        number1 = BigNumber("100", 10)
+        number2 = BigNumber("6", 10)
+
+        with self.assertRaises(errors.NumberSizeGreaterThanLimit, msg="pow overflow result failed"):
+            pow(number1,number2)
+
+    def test_root_simple(self):
+        number1 = BigNumber("10000", 10)
+        number2 = BigNumber("2", 10)
+        number3 = BigNumber("100", 10)
+
+        self.assertEqual(number1.root(number2), number3, "Number root simple failed")
+
+    def test_root_aprox(self):
+        number1 = BigNumber("5", 10)
+        number2 = BigNumber("2", 10)
+        number3 = BigNumber("2", 10)
+
+        self.assertEqual(number1.root(number2), number3, "Number root aprox failed")
+
+    def test_string_valid(self):
+        number1 = BigNumber("123123000123123", 100)
+
+        self.assertEqual(number1.value, "123123000123123", "Number string valid failed")
+
+    def test_string_null(self):
+
+        with self.assertRaises(errors.InvalidCharInNumberString, msg="null string verification failed"):
+            number1 = BigNumber("", 100)
+
 
 TEST_CASES = [TestBigNumberInit, TestBigNumberOperations]
 
